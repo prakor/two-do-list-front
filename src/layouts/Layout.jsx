3e3066788d015b8ea3bd0b13bfeef848b23cd276
gorr_w <prakitmoonkam@gmail.com>
2025-10-26 23:08:59 +0700

    Add logo image for the to-do list application

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom"
 const Layout = () => {
   return (
-    <div className="h-screen p-6 text-3xl">
+    <div className="h-screen p-6">
       <Outlet/>
     </div>
   )
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,21 +1,49 @@
 import { NavLink } from "react-router-dom";
 import "./Login.css";
-// import logo from "@/assets/images/logo/to-do-list.png";
+import logoImage from "@assets/images/logo/list.png";
 
 const Login = () => {
   return (
-    <div className="flex flex-col justify-between gap-8">
+    <div className="flex flex-col justify-between gap-8 pl-4 pr-4">
       <div className="header-title">
-        <NavLink to="/">
-          <div>
-            {/* <img src={logo} alt="icon"/> */}
-            Two door list
+        <NavLink to="/login">
+          <div className="flex flex-row items-center gap-2">
+            <img src={logoImage} alt="icon" width="32px" />
+            <div className="text-2xl font-semibold text-amber-500">
+              Two door list
+            </div>
           </div>
         </NavLink>
       </div>
       <div className="flex flex-row justify-between gap-4 mt-4">
-        <div className="">Login</div>
-        <div className="">IMG</div>
+        <div className="w-full">
+          <div className="login-title">Log in</div>
+          <form action="">
+            {/* login */}
+            <div>
+              <span>Email</span>
+              <div>
+                <input type="text" placeholder="Enter your email..." />
+              </div>
+            </div>
+            {/* password */}
+            <div>
+              <span>Password</span>
+              <div>
+                <input type="password" placeholder="Enter your password..." />
+              </div>
+            </div>
+            <button>Log in</button>
+          </form>
+          <hr />
+          <div className="flex flex-row justify-between">
+            <div>Don't have an account?</div>
+            <div>
+              <NavLink to="/register">Sign up</NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="w-full">IMG</div>
       </div>
     </div>
   );
