b416881575a8de8b309f67873585284f58a5320b
gor <prakitmoonkam@gmail.com>
2026-05-06 18:04:26 +0700

    feat: implement collapsible sidebar functionality with dynamic width adjustment

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,3 +1,5 @@
+// src/components/Sidebar/Sidebar.jsx
+import { useState, useRef, useEffect } from "react";
 import { NavLink } from "react-router";
 import { FaRegUserCircle, FaInbox, FaCalendarAlt } from "react-icons/fa";
 import { FiSidebar } from "react-icons/fi";
@@ -7,10 +9,33 @@ import { CiSearch } from "react-icons/ci";
 import "./Sidebar.css";
 
 const Sidebar = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [width, setWidth] = useState(280); // ความกว้างเริ่มต้น (px)
+  const [isResizing, setIsResizing] = useState(false);
+
+  const sidebarRef = useRef(null);
+  const minWidth = 220;
+  const maxWidth = 420;
+
+  // Toggle Collapse
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
   return (
-    <nav className="h-screen w-95 flex flex-row bg-gray-50 relative">
+    <nav
+      // className="h-screen w-95 flex flex-row bg-gray-50 relative"
+      ref={sidebarRef}
+      className={`h-screen bg-gray-50 relative flex-shrink-0 transition-all duration-200 border-r border-gray-200 transition duration-400 ease-out`}
+      style={{
+        width: isCollapsed ? "0px" : `${width}px`,
+      }}
+    >
       {/* side bar */}
-      <div className="w-full">
+      <div
+        // className="w-full"
+        className={`h-full overflow-hidden ${isCollapsed ? "hidden" : "block"}`}
+        // className={`h-full overflow-hidden transition-all duration-200 ${isCollapsed ? "opacity-0 pointer-events-none w-0" : "opacity-100 w-full" }`}
+      >
         <div className="flex flex-row justify-between m-2">
           <button className="flex flex-row items-center p-2 rounded-md gap-2 hover:bg-gray-200 cursor-pointer">
             <div className="avatar">
@@ -22,7 +47,10 @@ const Sidebar = () => {
             <IoIosArrowDown />
           </button>
           <div className="flex flex-row items-center gap-2">
-            <button className="btn btn-ghost btn-sm rounded-md p-2 hover:bg-gray-200 cursor-pointer">
+            <button
+              onClick={toggleCollapse}
+              className="btn btn-ghost btn-sm rounded-md p-2 hover:bg-gray-200 cursor-pointer"
+            >
               <FiSidebar />
             </button>
           </div>
@@ -110,6 +138,14 @@ const Sidebar = () => {
         </div>
       </div>
       <div className="handle-navigator bg-gray-50 hover:bg-gray-200"></div>
+      {isCollapsed && (
+        <button
+          onClick={toggleCollapse}
+          className="p-3 hover:bg-gray-200 rounded-md cursor-pointer transition-discrete duration-200 ease-out"
+        >
+          <FiSidebar className="text-xl" />
+        </button>
+      )}
     </nav>
   );
 };
